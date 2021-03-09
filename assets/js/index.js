345a1eb561d58ec1dc0b002ecaa15eaf4a702bb6
DominicSpears <dominicspears24@gmail.com>
2021-03-09 17:07:32 +0000

    add reset and instruction btn js

--- assets/js/index.js
+++ assets/js/index.js
@@ -58,3 +58,26 @@ function resetBoard() {
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+
+//function resetGame() {
+  //  [hasFlippedCard, lockBoard] = [false, false];
+    //[firstCard, secondCard] = [null, null];
+//}
+
+var modal = document.getElementById("instructionsModal");
+var btn = document.getElementById("instructions-btn");
+var span = document.getElementsByClassName("close-btn")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
